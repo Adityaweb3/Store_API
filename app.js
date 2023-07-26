6bd5c078bb8c2dcf3c9f39ff143b17574d3e86fb
Adityaweb3 <ap7596112@gmail.com>
2023-07-26 18:18:07 +0530

    db connect

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log('04 Store API')
+require('dotenv').config() ;
+
+const express = require('express')
+const app = express()
+
+
+const connectDB = require('./db/connect')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Store API</h1>')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+
+
+
+const port = process.env.PORT || 5000 
+
+const start = async()=>{
+    try {
+
+        await connectDB(process.env.MONGO_URI) 
+
+        app.listen(port , console.log(`listening at port  ${port}`))
+
+    }
+
+    catch(error) {
+
+        console.log(error)
+
+    }
+}
+
+start()
+
+
