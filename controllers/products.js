d9bc60d1104918d4e10f5fd8a0d064e0c292cf34
Adityaweb3 <ap7596112@gmail.com>
2023-07-28 14:54:33 +0530

    sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,62 @@
-
+const Product = require('../models/product')
 
 const getAllProductsStatic = async(req, res)=>{
 
-    throw new ERROR ('testing async error')
+    const products = await Product.find({name : "vase table"})
 
-    res.status(200).json({msg : 'product testing route'})
+    res.status(200).json({products, nbHits : products.length})
 }
 
 const getAllProducts = async(req,res) =>{
-    res.status(200).json({msg : 'product route'})
+
+    const {featured, company, name, sort, fields} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured ==='true'? true : false ;
+    }
+
+    if(company){
+        queryObject.company = company ;
+    }
+
+    if(name){
+        queryObject.name = {$regex : name , $options : 'i'} ;
+    }
+
+   // console.log(queryObject)
+
+
+
+    let result = Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result =  result.sort(sortList)
+
+        
+    }
+
+    else {
+        result = result.sort('createdAt')
+    }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result =  result.select(fieldsList)
+
+    }
+
+    const page = Number(req.query.page) || 1 
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit ;
+
+
+    result = result.skip(skip).limit(limit)
+
+
+
+    const products = await result 
+    res.status(200).json({products, nbHits : products.length})
 
 
 }
